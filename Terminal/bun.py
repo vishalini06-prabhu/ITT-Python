47d4aa775cda108f9de34c10817fd8e2dc437533
vishalini06-prabhu <shunmugaprabhu_bcs28@mepcoeng.ac.in>
2026-04-20 11:13:01 +0530

    Create bun.py

---
+++ Terminal/bun.py
@@ -0,0 +1,7 @@
+num = int(input("Enter the value for N : "))
+if num%4 == 0 :
+   print("No , The  sandwich can't be made ")
+elif num%4 >= 2 :
+   print("Yes , the sandwhich can be made ")
+else :
+   print(" No , The  sandwich can't be made ")
