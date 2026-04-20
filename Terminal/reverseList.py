759b87342c75a32df46432d0fc1c6720969dcf0c
Vishalini S <shunmugaprabhu_bcs28@mepcoeng.ac.in>
2026-04-20 12:01:25 +0530

    Add reverseList.py to reverse a user-input list
    The script takes a list input from the user and prints both the reversed list using the built-in reverse function and a manual reversal method.

---
+++ Terminal/reverseList.py
@@ -0,0 +1,6 @@
+l=list(eval(input("Enter list elements:")))
+print("Entered list:",l)
+rev=l[::-1]
+l.reverse()
+print("Reversed list using functions:",l)
+print("Reversed list  without using function:",rev)
